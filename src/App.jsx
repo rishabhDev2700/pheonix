e8b9475adeef7c0baa939ef3ff25a171f34e0e3a
rishabhDev2700 <rishavkumar2700@gmail.com>
2023-10-15 14:41:08 +0530

    Added Router and homepage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import './components/button/button.css'
-import { Footer } from './components/footer/footer';
-import { Form } from './components/form/form';
-import { Socials } from './components/socials/socials';
-// import { ProjectTile } from './components/project-tile/project-tile';
-import { Title } from './components/title/title';
-import {Topbar} from './components/topbar/topbar'
+import { Route, Routes } from '@solidjs/router'
+import { Topbar } from './components/topbar/topbar';
+import { Homepage } from './pages/homepage';
 
 function App() {
   return (
-    <div>
-      <Topbar/>
-      {/* <button class="submit-button">Send the Message</button> */}
-      {/* <ProjectTile title="Foodie's Stop" description="Just some random data for describing the project" image="https://www.creativeswall.com/wp-content/uploads/2014/03/Branding-Identity-MockUp-Vol8-full.jpg"/> */}
-      {/* <Title>Projects</Title> */}
-      {/* <Form/> */}
-      {/* <Footer/> */}
-    </div>
+    <>
+      <Topbar />
+      <Routes>
+        <Route path='/' component={Homepage} />
+      </Routes>
+    </>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
-
+import {Router} from '@solidjs/router'
 import './index.css';
 import App from './App';
 
@@ -12,4 +12,11 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   );
 }
 
-render(() => <App />, root);
+render(
+  () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  )
+}, root);
